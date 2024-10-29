7567f21c2562ffa25d96ec0693dcde3d5bc6a859
Adrian Valdes <adrianvaldeshdz@gmail.com>
2024-10-29 14:54:49 +0100

    feat: test for scope conventional commits

--- src/015-essential-types-and-annotations/035-pass-types-to-set.problem.ts
+++ src/015-essential-types-and-annotations/035-pass-types-to-set.problem.ts
@@ -1,16 +1,14 @@
 // CODE
 
-
-
 const userIds = new Set<number>();
 
-// TESTS
+// TESTS This is a test
 
 userIds.add(1);
 userIds.add(2);
 userIds.add(3);
 
 // @ts-expect-error
-userIds.add("123");
+userIds.add('123');
 // @ts-expect-error
-userIds.add({ name: "Max" });
+userIds.add({ name: 'Max' });
